9e3bd209b469197c53ee848b4f73ef90d70f36f4
Ryan Farrell <farrell@cs.byu.edu>
2021-12-21 11:14:17 -0700

    initial brett/ryan images

---
+++ staff-images/resize_to_fit.py
@@ -0,0 +1,15 @@
+from sys import argv
+import os
+from PIL import Image
+
+semester = argv[1]
+for image in os.listdir(semester):
+    path = os.path.join(semester, image)
+    im = Image.open(path)
+    print(path, im.size)
+    if max(im.size) <= 256:
+        continue
+    wratio = 256 / max(im.size)
+    im = im.convert('RGB')
+    im = im.resize((int(wratio * im.size[0]), int(wratio * im.size[1])))
+    im.save(path)
